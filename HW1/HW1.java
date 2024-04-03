14b0616f285ea1a2e3132c32b68dcb50e4bf2414
TudRVN <nongvandenhoc@gmail.com>
2024-04-03 23:07:55 +0700

    Create HW1.java

---
+++ HW1/HW1.java
@@ -0,0 +1,16 @@
+public class HW1 {
+    public static void main(String[] args) {
+
+        System.out.println("Họ Tên: Nguyễn Hoàng Tú");
+        System.out.println("MSV: 23020428");
+        System.out.println("Lớp: K68-AI2 (Tích cực)");
+        System.out.println("Username: nguyenhoangtu3241");
+        System.out.println("Email: nguyenhoangtu3241@gmail.com");
+
+        for (int i = 9; i >= 1; i--) {
+            System.out.println(i + " bottles of beer on the wall, 9 bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
